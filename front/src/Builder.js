5b8ba5e8767f5a8e574937d91f77810a7849637f
Arthur Costa <arthurcosta@Arthurs-MacBook-Pro.local>
2018-08-29 18:42:53 -0300

    Style changes and Json download

--- front/src/Builder.js
+++ front/src/Builder.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Builder.css';
 
 class HeaderField extends Component {
+  // label, mandatory, curriculum, stateChanger
   capitalize(word) {
     return word.charAt(0).toUpperCase() + word.slice(1)
   }
@@ -16,6 +17,23 @@ class HeaderField extends Component {
   }
 }
 
+class CvItemForm extends Component {
+  // label, hidden
+  render() {
+    if (this.props.hide){
+      return  <div>
+                <div className="Base-subtitle">Work:</div><br/>
+                <div className="Base-button"><a>Add</a></div><br/>
+              </div>
+    } else {
+      return  <div>
+                <div className="Base-subtitle">Worky:</div><br/>
+                <div className="Base-button"><a>Add</a></div><br/>
+              </div>
+    }
+  }
+}
+
 class Builder extends Component {
     constructor(props) {
       super(props)
@@ -27,6 +45,7 @@ class Builder extends Component {
         }
       }
       this.handleChangeHeader = this.handleChangeHeader.bind(this)
+      this.downloadCvAsJson = this.downloadCvAsJson.bind(this)
     }
 
     handleChangeHeader(event) {
@@ -35,6 +54,13 @@ class Builder extends Component {
       this.setState({curriculum: aux})
     }
 
+    downloadCvAsJson() {
+      var element = document.createElement("a");
+      var file = new Blob([JSON.stringify(this.state.curriculum)], {type: 'text/plain'});
+      element.href = URL.createObjectURL(file);
+      element.download = "cv.json";
+      element.click();
+    }
     render() {
        return <div className="Base">
                   <div className="Base-title">Curriculum Vitae:</div><br/>
@@ -47,16 +73,18 @@ class Builder extends Component {
                   <HeaderField stateChanger={this.handleChangeHeader} curriculum={this.state.curriculum} label="homepage" mandatory={false}/>
                   <HeaderField stateChanger={this.handleChangeHeader} curriculum={this.state.curriculum} label="Address" mandatory={false}/>
                   <HeaderField stateChanger={this.handleChangeHeader} curriculum={this.state.curriculum} label="Birthday" mandatory={false}/>
-                  <div className="Base-subtitle">Work:</div><br/>
-                  <div className="Base-button"><a>Add</a></div><br/>
+                  <CvItemForm hide={true}/>
                   <div className="Base-subtitle">Education:</div><br/>
                   <div className="Base-button"><a>Add</a></div><br/>
                   <div className="Base-subtitle">Languages:</div><br/>
                   <div className="Base-button"><a>Add</a></div><br/>
-                  <div className="Base-subtitle">Academic Experience:</div><br/>
+                  <div className="Base-subtitle">Academic:</div><br/>
+                  <div className="Base-button"><a>Add</a></div><br/>
+                  <div className="Base-subtitle">Project:</div><br/>
                   <div className="Base-button"><a>Add</a></div><br/>
                   <div className="Base-subtitle">Achievements:</div><br/>
-                  <div className="Base-button"><a>Add</a></div><br/>
+                  <div className="Base-button"><a>Add</a></div><br/><br/>
+                  <div className="Base-button"><a onClick={this.downloadCvAsJson}>Json Download</a></div><br/>
               </div>
        }
  }
