9a01255b22b92584e68a1e26e4ae9e0a07bf3faf
Arthur Costa <arthurcosta@Arthurs-MacBook-Pro.local>
2018-08-29 08:07:19 -0300

    Starting Front end support`

---
+++ front/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+import Builder from './Builder';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {tab: 1}
+  }
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to CVCS</h1>
+        </header>
+        <div className="App-sidenav">
+          <a onClick={() => { this.setState({tab: 1}) }}>Test Me</a>
+          <a onClick={() => { this.setState({tab: 2}) }}>Log in (Later)</a>
+          <a onClick={() => { this.setState({tab: 3}) }}>About The Project</a>
+        </div>
+        <div className="App-intro">
+          { this.state.tab === 1 ? <Builder> </Builder> : null }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ front/src/Builder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Builder.css';
+
+class Builder extends Component{
+    constructor(props) {
+      super(props)
+      this.state = {curriculum: {}}
+    }
+    render() {
+       return <div className="Base">
+                 OIE
+              </div>
+       }
+ }
+
+export default Builder;
