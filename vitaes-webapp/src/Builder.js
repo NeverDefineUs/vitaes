f40fbcc4bd83338106bf0103297556e8e1091ed5
Arthurlpgc <arthurlpgcosta@gmail.com>
2018-09-05 23:02:52 -0300

    change protocol

--- vitaes-webapp/src/Builder.js
+++ vitaes-webapp/src/Builder.js
@@ -192,7 +192,7 @@ class Builder extends Component {
       if (this.hostname == 'vitaes.io:5000') {
         this.hostname = 'renderer.vitaes.io'
       }
-      fetch('http://' + this.hostname + '/CVTYPES/', {
+      fetch( window.location.protocol + '//' + this.hostname + '/CVTYPES/', {
         method: 'GET',
         headers: {
           'Accept': 'application/json',
@@ -259,7 +259,7 @@ class Builder extends Component {
     downloadCvAsPDF() {
       var db = firebase.database().ref('cv-dumps').push()
       db.set(this.props.cv)
-      fetch('http://' + this.hostname + '/CV/', {
+      fetch( window.location.protocol + '//' + this.hostname + '/CV/', {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
