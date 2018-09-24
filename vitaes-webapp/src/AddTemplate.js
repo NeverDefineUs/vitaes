04149b3bc663f905e5106199546daa0caf03ee40
Arthurlpgc <arthurlpgcosta@gmail.com>
2018-09-23 23:27:29 -0300

    Add template field class

--- vitaes-webapp/src/AddTemplate.js
+++ vitaes-webapp/src/AddTemplate.js
@@ -1,4 +1,24 @@
 import React, { Component } from 'react'
+class TemplateField extends Component {
+  // label, placeholder, value, callback
+  render() {
+    return (
+      <div className="Base-field">
+        <div className="Base-label">
+          {this.props.label}:
+        </div>
+        <input 
+          type="text"
+          name={this.props.label}
+          value={this.props.value}
+          className="Base-inputfield"
+          placeholder={this.props.placeholder}
+          onChange={this.props.callback}
+        />
+      </div>
+    )
+  }
+}
 
 class AddTemplate extends Component {
     render() {
