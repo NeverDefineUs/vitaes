c20265ec3f843e801128a29b17fbfc448d893d56
Arthurlpgc <arthurlpgcosta@gmail.com>
2019-02-20 23:53:30 -0300

    add sleeps

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 docker volume rm mongodb
 docker volume create --name=mongodb
 docker-compose up -d mongo
+sleep 20
 docker-compose exec mongo mongorestore --archive=mbckp/base.archive --username root --password vitaes
+sleep 2
 docker-compose down
