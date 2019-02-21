e4cd68bcb3baadad59d349bf42a6fa82bc0ab788
Arthurlpgc <arthurlpgcosta@gmail.com>
2019-02-20 22:10:48 -0300

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 docker volume create --name=mongodb
 docker-compose build
-docker-compose exec mongo bash
+docker-compose exec mongo mongorestore --archive=mbckp/base.archive --username root --password vitaes
