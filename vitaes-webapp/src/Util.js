8962f108c9580faa5e2ea9ecf91b4969949f2a52
Arthurlpgc <arthurlpgcosta@gmail.com>
2019-03-06 01:22:03 -0300

    fixes

--- vitaes-webapp/src/Util.js
+++ vitaes-webapp/src/Util.js
@@ -1,5 +1,5 @@
-export function capitalize(word) {
-  word = word.replace('_', ' ');
+export function capitalize(rawWord) {
+  const word = rawWord.replace('_', ' ');
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
@@ -15,12 +15,12 @@ export function copyElement(element) {
   return JSON.parse(JSON.stringify(element));
 }
 
-export function removeDisabled(cv) {
-  cv = copyElement(cv);
-  for (const key in cv) {
+export function removeDisabled(rawCv) {
+  const cv = copyElement(rawCv);
+  Object.keys(cv).forEach((key) => {
     if (Array.isArray(cv[key])) {
       cv[key] = cv[key].filter(element => !element.disable);
     }
-  }
+  });
   return cv;
 }
