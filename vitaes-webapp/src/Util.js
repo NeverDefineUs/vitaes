1300162d0b00eeaea0a6d039614e3801ebf92f28
Arthurlpgc <arthurlpgcosta@gmail.com>
2019-03-11 18:37:44 -0300

    fix date validation

--- vitaes-webapp/src/Util.js
+++ vitaes-webapp/src/Util.js
@@ -56,7 +56,7 @@ export function validateEmail(email) {
 }
 
 export function validateDate(dateStr) {
-  const date = new Date(dateStr);
+  const date = Date.parse(dateStr);
   const day = Number(dateStr[8] + dateStr[9]);
   const month = Number(dateStr[5] + dateStr[6]);
   const year = Number(dateStr[0] + dateStr[1] + dateStr[2] + dateStr[3]);
