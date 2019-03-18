bdeb6586a4a81456af32bdfff01b4954bc561855
Arthurlpgc <arthurlpgcosta@gmail.com>
2019-03-18 20:56:07 -0300

    remove disable tag

--- vitaes-webapp/src/utils/removeDisabled.js
+++ vitaes-webapp/src/utils/removeDisabled.js
@@ -5,7 +5,14 @@ export default function removeDisabled(rawCv) {
   Object.keys(cv).forEach((key) => {
     if (Array.isArray(cv[key])) {
       cv[key] = cv[key].filter(element => !element.disable);
+      cv[key] = cv[key].map((element) => {
+        let elementCopy = copyElement(element);
+        elementCopy.disable = true;
+        delete elementCopy.disable;
+        return elementCopy;
+      });
     }
   });
+
   return cv;
 }
