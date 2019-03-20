db5bc2ea6a5e5ac695165bf0e6c23f2d409fc354
Arthurlpgc <arthurlpgcosta@gmail.com>
2019-03-20 19:47:42 -0300

    quick fix

--- vitaes-webapp/src/utils/removeDisabled.js
+++ vitaes-webapp/src/utils/removeDisabled.js
@@ -1,12 +1,12 @@
-import { deepCopy } from 'lodash';
+import _ from 'lodash';
 
 export default function removeDisabled(rawCv) {
-  const cv = deepCopy(rawCv);
+  const cv = _.cloneDeep(rawCv);
   Object.keys(cv).forEach((key) => {
     if (Array.isArray(cv[key])) {
       cv[key] = cv[key].filter(element => !element.disable);
       cv[key] = cv[key].map((element) => {
-        const elementCopy = deepCopy(element);
+        const elementCopy = _.cloneDeep(element);
         elementCopy.disable = true;
         delete elementCopy.disable;
         return elementCopy;
