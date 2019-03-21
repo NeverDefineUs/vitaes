b327ddd6c109c9416f1deaf621998cb047d3e526
Rafael Albuquerque <rafaellsma@gmail.com>
2019-03-21 01:42:54 -0300

    Replace hrefs in NavBar

--- vitaes-webapp/src/App.js
+++ vitaes-webapp/src/App.js
@@ -1,46 +1,26 @@
 import React, { Component } from 'react';
 
 import firebase from 'firebase';
-import { ToastContainer, toast } from 'react-toastify';
 import {
   Navbar, Nav, NavDropdown, Row, Col, Container,
 } from 'react-bootstrap';
 
-import getHostname from 'utils/getHostname';
 import { translate, setLocale } from 'i18n/locale';
 
-import About from './About';
-import AddTemplate from './AddTemplate';
-import Builder from './Builder';
 import Login from './Login';
-import TemplateHub from './TemplateHub';
 import config from './config';
 import { setupAlerts } from './AlertManager/util';
-import AlertManager from './AlertManager';
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
+import AppRouter from './AppRouter';
 
 
 firebase.initializeApp(config);
-
-const testCv = {
-  CvHeaderItem: {
-    name: '',
-  },
-  CvWorkExperienceItem: [],
-  CvAcademicProjectItem: [],
-  CvImplementationProjectItem: [],
-  CvAchievementItem: [],
-  CvEducationalExperienceItem: [],
-  CvLanguageItem: [],
-};
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tab: 1,
-      cv: testCv,
       user: null,
       permissions: null,
       showLogin: false,
@@ -48,69 +28,16 @@ class App extends Component {
     this.showLogin = this.showLogin.bind(this);
     this.hideLogin = this.hideLogin.bind(this);
     this.logout = this.logout.bind(this);
-    this.cvSetter = this.cvSetter.bind(this);
-    const app = this;
     setupAlerts();
     const dbHP = firebase.database().ref('permissions');
     dbHP.on('value', (snapshot) => {
-      app.setState({ permissions: snapshot.val() });
+      this.setState({ permissions: snapshot.val() });
     });
-    firebase
-      .auth()
-      .getRedirectResult()
-      .then(() => {
-        const user = firebase.auth().currentUser;
-        const db = firebase
-          .database()
-          .ref('cvs')
-          .child(user.uid);
-        app.setState({ user, tab: 1 });
-        const loadingToast = toast.info(`${translate('loading')}...`, { autoClose: false });
-        db.on(
-          'value',
-          (snapshot) => {
-            toast.dismiss(loadingToast);
-            const snap = snapshot.val();
-            if (snap === null) {
-              db.set(testCv);
-            } else {
-              app.setState({ cv: snap });
-            }
-          },
-          () => {
-          },
-        );
-      })
-      .catch(() => {
-        app.showLogin();
-      });
-
-    fetch(`${window.location.protocol}//${getHostname()}/template/`, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    }).then((response) => {
-      if (response.ok) {
-        const jsonPromise = response.json();
-        jsonPromise.then((json) => {
-          this.setState({ cv_models: json });
-        });
-      } else {
-        const textPromise = response.text();
-        textPromise.then(text => toast.error(`${translate('error')}: ${text}`));
-      }
-    });
-  }
-
-  cvSetter(cv) {
-    this.setState({ cv });
   }
 
   logout() {
     firebase.auth().signOut();
-    this.setState({ user: null, tab: 1, cv: testCv });
+    this.setState({ user: null });
   }
 
   showLogin() {
@@ -133,122 +60,92 @@ class App extends Component {
       />
     );
     return (
-      <span>
-        <ToastContainer position="bottom-right" />
-        <div>
-          <script
-            src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-            integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-            crossOrigin="anonymous"
-          />
-          <Navbar collapseOnSelect expand="lg" fixed="top" bg="dark" variant="dark">
-            <Navbar.Brand href="#home">
-              <img
-                alt=""
-                src="/mod5.svg"
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-              />
-              {' Vitaes'}
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link
-                  href="#create_cv"
-                  onClick={() => {
-                    this.setState({ tab: 1 });
-                  }}
-                >
-                  {translate('create_cv')}
+      <div>
+        <Navbar collapseOnSelect expand="lg" fixed="top" bg="dark" variant="dark">
+          <Navbar.Brand href="/">
+            <img
+              alt=""
+              src="/mod5.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+            {' Vitaes'}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link
+                href="/"
+                onClick={() => {
+                  this.setState({ tab: 1 });
+                }}
+              >
+                {translate('create_cv')}
+              </Nav.Link>
+              {this.state.user !== null
+                && this.state.permissions !== null
+                && this.state.permissions[this.state.user.uid]
+                ? (
+                  [
+                    <Nav.Link
+                      href="/create-template"
+                      onClick={() => {
+                        this.setState({ tab: 4 });
+                      }}
+                    >
+                      {translate('create_template')}
+                    </Nav.Link>,
+                    <Nav.Link
+                      href="/alert-manager"
+                      onClick={() => {
+                        this.setState({ tab: 5 });
+                      }}
+                    >
+                      {translate('alert_manager')}
+                    </Nav.Link>,
+                  ]
+                ) : null}
+              <Nav.Link
+                href="/hub"
+                onClick={() => {
+                  this.setState({ tab: 2 });
+                }}
+              >
+                Template Hub
                 </Nav.Link>
-                {this.state.user !== null
-                  && this.state.permissions !== null
-                  && this.state.permissions[this.state.user.uid]
-                  ? (
-                    [
-                      <Nav.Link
-                        href="#create_template"
-                        onClick={() => {
-                          this.setState({ tab: 4 });
-                        }}
-                      >
-                        {translate('create_template')}
-                      </Nav.Link>,
-                      <Nav.Link
-                        href="#alert_manager"
-                        onClick={() => {
-                          this.setState({ tab: 5 });
-                        }}
-                      >
-                        {translate('alert_manager')}
-                      </Nav.Link>,
-                    ]
-                  ) : null}
-                <Nav.Link
-                  href="#hub"
-                  onClick={() => {
-                    this.setState({ tab: 2 });
-                  }}
-                >
-                  Template Hub
-                </Nav.Link>
-                <Nav.Link
-                  href="#about"
-                  onClick={() => {
-                    this.setState({ tab: 3 });
-                  }}
-                >
-                  {translate('about_the_project')}
-                </Nav.Link>
-                <NavDropdown title={translate('language')} id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#lang/en" onClick={() => { setLocale('en_US'); this.setState({}); }}>English</NavDropdown.Item>
-                  <NavDropdown.Item href="#lang/pt" onClick={() => { setLocale('pt_BR'); this.setState({}); }}>Português</NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-              <Nav className="mr-sm-2">
-                {this.state.user !== null ? (
-                  <Nav.Link href="#signout" onClick={this.logout}>{translate('sign_out')}</Nav.Link>
-                ) : (
-                  <Nav.Link href="#signin" onClick={this.showLogin}>{translate('sign_in')}</Nav.Link>
+              <Nav.Link
+                href="/about"
+                onClick={() => {
+                  this.setState({ tab: 3 });
+                }}
+              >
+                {translate('about_the_project')}
+              </Nav.Link>
+              <NavDropdown title={translate('language')} id="basic-nav-dropdown">
+                <NavDropdown.Item onClick={() => { setLocale('en_US'); this.setState({}); }}>English</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => { setLocale('pt_BR'); this.setState({}); }}>Português</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav className="mr-sm-2">
+              {this.state.user !== null ? (
+                <Nav.Link onClick={this.logout}>{translate('sign_out')}</Nav.Link>
+              ) : (
+                  <Nav.Link onClick={this.showLogin}>{translate('sign_in')}</Nav.Link>
                 )}
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          <br />
-          {login}
-          <Container>
-            <Row className="justify-content-md-center">
-              <Col>
-                {this.state.tab === 1 ? (
-                  <Builder
-                    cv_models={this.state.cv_models}
-                    cv={this.state.cv}
-                    cvSetter={this.cvSetter}
-                    user={this.state.user}
-                  >
-                    {' '}
-                  </Builder>
-                ) : null}
-                {this.state.tab === 2 ? <TemplateHub user={this.state.user} /> : null}
-                {this.state.tab === 3 ? <About /> : null}
-                {this.state.tab === 4 ? (
-                  <AddTemplate cv_models={this.state.cv_models} />
-                ) : null}
-                {this.state.tab === 5 ? (
-                  <AlertManager />
-                ) : null}
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </span>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <br />
+        {login}
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col>
+              <AppRouter />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
