e81daf0d0dba4f2c9cc35f0a11767ceeeb16b815
Rafael Albuquerque <rafaellsma@gmail.com>
2019-03-21 00:41:59 -0300

    Fix AddTemplate to encapsulate data loading

---
+++ vitaes-webapp/src/AddTemplate/AddTemplate.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import firebase from 'firebase';
+import { getEmptyTemplate } from './util';
+import TemplateField from './TemplateField';
+import OwnedTemplate from './OwnedTemplate';
+
+class AddTemplate extends Component {
+  constructor() {
+    super();
+    this.state = { template: getEmptyTemplate() };
+  }
+
+  render() {
+    const ownedCvs = Object.values(this.props.cv_models).filter(template =>
+      (template.owner === firebase.auth().currentUser.uid));
+    const ownedCvsNodes = ownedCvs.map(template =>
+      (<OwnedTemplate template={template} key={template} />));
+
+    return (
+      <div className="Base">
+        <h1>Create a template:</h1>
+        <TemplateField
+          placeholder="awesome"
+          label="Name"
+          value={this.state.name}
+          callback={(e) => {
+            const { template } = this.state;
+            template.name = e.target.value;
+            this.setState({ template });
+          }}
+        />
+        <TemplateField
+          placeholder="pdflatex"
+          label="Command"
+          value={this.state.command}
+          callback={(e) => {
+            const { template } = this.state;
+            template.command = e.target.value;
+            this.setState({ template });
+          }}
+        />
+        <h2>Params:</h2>
+        <div className="Base-button">
+          <a
+            href="#"
+            onClick={() => {
+              fetch('http://localhost:5000/template/', {
+                method: 'POST',
+                headers: {
+                  Accept: 'application/json',
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(this.state.template),
+              });
+              this.setState({ template: getEmptyTemplate() });
+            }}
+          >
+            Submit
+          </a>
+        </div>
+        <div className="Base-button">
+          <a href="#" onClick={() => { }}>Add New Param</a>
+        </div>
+        <hr style={{ marginTop: '3em' }} />
+        {ownedCvsNodes}
+      </div>
+    );
+  }
+}
+
+export default AddTemplate;
--- vitaes-webapp/src/AddTemplate/index.js
+++ vitaes-webapp/src/AddTemplate/index.js
@@ -1,71 +1,44 @@
 import React, { Component } from 'react';
-import firebase from 'firebase';
-import { getEmptyTemplate } from './util';
-import TemplateField from './TemplateField';
-import OwnedTemplate from './OwnedTemplate';
+import { ToastContainer, toast } from 'react-toastify';
 
-class AddTemplate extends Component {
-  constructor() {
-    super();
-    this.state = { template: getEmptyTemplate() };
+import getHostname from 'utils/getHostname';
+import { translate } from 'i18n/locale';
+
+import AddTemplate from './AddTemplate'
+
+class AddTemplateContainer extends Component {
+  state = {
+    cv_models: []
+  }
+
+  componentDidMount() {
+    fetch(`${window.location.protocol}//${getHostname()}/template/`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => {
+      if (response.ok) {
+        const jsonPromise = response.json();
+        jsonPromise.then((json) => {
+          this.setState({ cv_models: json });
+        });
+      } else {
+        const textPromise = response.text();
+        textPromise.then(text => toast.error(`${translate('error')}: ${text}`));
+      }
+    });
   }
 
   render() {
-    const ownedCvs = Object.values(this.props.cv_models).filter(template =>
-      (template.owner === firebase.auth().currentUser.uid));
-    const ownedCvsNodes = ownedCvs.map(template =>
-      (<OwnedTemplate template={template} key={template} />));
-
     return (
-      <div className="Base">
-        <h1>Create a template:</h1>
-        <TemplateField
-          placeholder="awesome"
-          label="Name"
-          value={this.state.name}
-          callback={(e) => {
-            const { template } = this.state;
-            template.name = e.target.value;
-            this.setState({ template });
-          }}
-        />
-        <TemplateField
-          placeholder="pdflatex"
-          label="Command"
-          value={this.state.command}
-          callback={(e) => {
-            const { template } = this.state;
-            template.command = e.target.value;
-            this.setState({ template });
-          }}
-        />
-        <h2>Params:</h2>
-        <div className="Base-button">
-          <a
-            href="#"
-            onClick={() => {
-              fetch('http://localhost:5000/template/', {
-                method: 'POST',
-                headers: {
-                  Accept: 'application/json',
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(this.state.template),
-              });
-              this.setState({ template: getEmptyTemplate() });
-            }}
-          >
-            Submit
-          </a>
-        </div>
-        <div className="Base-button">
-          <a href="#" onClick={() => { }}>Add New Param</a>
-        </div>
-        <hr style={{ marginTop: '3em' }} />
-        {ownedCvsNodes}
-      </div>
-    );
+      <React.Fragment>
+        <ToastContainer position="bottom-right" />
+        <AddTemplate cv_models={this.state.cv_models} />
+      </React.Fragment>
+    )
   }
 }
 
-export default AddTemplate;
+export default AddTemplateContainer;
