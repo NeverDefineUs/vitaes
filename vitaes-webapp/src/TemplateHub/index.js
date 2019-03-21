4dccc24fd14b70d14b9719dd9d32ec1e52b86a7d
Rafael Albuquerque <rafaellsma@gmail.com>
2019-03-21 00:34:27 -0300

    Fix TemplateHub to encapsulate data loading

--- vitaes-webapp/src/TemplateHub/index.js
+++ vitaes-webapp/src/TemplateHub/index.js
@@ -1,60 +1,25 @@
-import React, { Component } from 'react';
-import { toast } from 'react-toastify';
+import React from 'react';
+import firebase from 'firebase';
 
-import TemplateHubModel from './TemplateHubModel';
+import TemplateHub from './TemplateHub'
 
-class TemplateHub extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { cv_models: {} };
-    this.hostname = `${window.location.hostname}:5000`;
-    if (this.hostname === 'vitaes.io:5000') {
-      this.hostname = 'renderer.vitaes.io';
-    }
-    this.fetchTemplates = this.fetchTemplates.bind(this);
-    this.fetchTemplates();
+class TemplateHubContainer extends React.Component {
+  state = {
+    user: null
   }
 
-  fetchTemplates() {
-    fetch(`${window.location.protocol}//${this.hostname}/template/`, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    }).then((response) => {
-      if (response.ok) {
-        const jsonPromise = response.json();
-        jsonPromise.then((json) => {
-          this.setState({ cv_models: json });
-        });
-      } else {
-        const textPromise = response.text();
-        textPromise.then(text => toast.error(`Error:${text}`));
-      }
-    });
+  componentDidMount() {
+    firebase
+      .auth()
+      .getRedirectResult()
+      .then(() => {
+        const user = firebase.auth().currentUser
+        this.setState({ user })
+      })
   }
 
   render() {
-    const templateList = [];
-    for (const key in this.state.cv_models) {
-      templateList.push(
-        <TemplateHubModel
-          fetchTemplates={this.fetchTemplates}
-          user={this.props.user}
-          keyName={key}
-          model={this.state.cv_models[key]}
-        />,
-      );
-    }
-    return (
-      <div className="Base">
-        <h1>Template Hub</h1>
-        {templateList}
-        <br />
-      </div>
-    );
+    return <TemplateHub user={this.state.user} />
   }
 }
-
-export default TemplateHub;
+export default TemplateHubContainer;
