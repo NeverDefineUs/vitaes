5744d7aec03d6e3b0b84b3117acb95a06afa4608
Arthurlpgc <arthurlpgcosta@gmail.com>
2019-04-06 15:45:07 -0300

    Fix: Renderer failing on unknown languages

--- vitaes-renderer/I18n.py
+++ vitaes-renderer/I18n.py
@@ -1,9 +1,12 @@
 import json, sys
 
 def get_raw_resources(lang):
-  with open('Resources/' + lang + '.json', encoding='utf-8') as json_file:  
-    data = json.load(json_file)
-    return data
+  try:
+    with open('Resources/' + lang + '.json', encoding='utf-8') as json_file:
+      data = json.load(json_file)
+      return data
+  except:
+    return {}
 
 
 def get_resources(lang):
