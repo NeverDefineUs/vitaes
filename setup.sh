e46d6d8c879aae439bd025fa8071224290861ad0
Arthurlpgc <arthurlpgcosta@gmail.com>
2019-05-23 00:52:29 -0300

    remove hooks

--- setup.sh
+++ setup.sh
@@ -9,5 +9,3 @@ sleep 20
 docker-compose exec mongo mongorestore --archive=mbckp/base.archive --username root --password vitaes
 sleep 2
 docker-compose down
-cp ./hooks/* ./.git/hooks/
-chmod +x .git/hooks/pre-commit
