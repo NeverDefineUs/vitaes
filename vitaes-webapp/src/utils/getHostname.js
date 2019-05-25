fdafe115a948daa719ba35042153ea08f9e8de7c
Ramon Saboya <ramonsaboya@fb.com>
2019-05-25 05:18:08 -0300

    update

--- vitaes-webapp/src/utils/getHostname.js
+++ vitaes-webapp/src/utils/getHostname.js
@@ -1,9 +1,19 @@
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost'
+    // [::1] is the IPv6 localhost address.
+    || window.location.hostname === '[::1]'
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    || window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/,
+    ),
+);
+
 const getHostname = (target, port) => {
   if (process.env.K8S) {
     return `${target}.k8s.vitaes.io`;
   }
 
-  if (process.env.NODE_ENV === 'development') {
+  if (isLocalhost) {
     return `localhost:${port}`;
   }
 
