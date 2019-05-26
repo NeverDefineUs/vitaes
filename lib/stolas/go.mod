c5ad8cd223b0d023fe0b13d5dfec756e58507b4c
Ramon Saboya <ramonsaboya@fb.com>
2019-05-26 05:35:00 -0300

    create stolas

---
+++ lib/stolas/go.mod
@@ -0,0 +1,5 @@
+module github.com/neverdefineus/vitaes/lib/stolas
+
+go 1.12
+
+require github.com/mattn/go-sqlite3 v1.10.0
---
+++ lib/stolas/stolas.go
@@ -0,0 +1,96 @@
+package stolas
+
+import (
+	"database/sql"
+	"log"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3" // sqlite
+)
+
+// Client vlogger client structure
+type Client struct {
+	sqliteClient *sql.DB
+}
+
+// NewClient initialize vlogger client
+func NewClient(dbFilePath string) (*Client, error) {
+	db, err := sql.Open("sqlite3", dbFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(createTableStmt)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		for {
+			_, err = db.Exec(deleteStaleStmt)
+			failOnError(err, "Failed to delete stale rows")
+			time.Sleep(10 * time.Minute)
+		}
+	}()
+
+	return &Client{
+		sqliteClient: db,
+	}, nil
+}
+
+// LogStep logs data
+func (c *Client) LogStep(email, cvHash, origin, step, data, stacktrace string) error {
+	stmt, err := c.sqliteClient.Prepare(logStmt)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(
+		stringOrNil(email),
+		stringOrNil(cvHash),
+		stringOrNil(origin),
+		stringOrNil(step),
+		stringOrNil(data),
+		stringOrNil(stacktrace),
+	)
+	return err
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func stringOrNil(value string) interface{} {
+	if value != "" {
+		return value
+	}
+	return nil
+}
+
+const createTableStmt = `
+CREATE TABLE IF NOT EXISTS "cv_gen_tracking" (
+	time TEXT DEFAULT(strftime('%Y-%m-%d %H-%M-%f','now')) NOT NULL,
+	email TEXT,
+	cv_hash TEXT,
+	origin TEXT NOT NULL,
+	step TEXT NOT NULL,
+	data TEXT,
+	stacktrace TEXT,
+	PRIMARY KEY (time, email, cv_hash)
+);`
+
+const deleteStaleStmt = `
+DELETE FROM "cv_gen_tracking" WHERE "time" < strftime('%Y-%m-%d %H-%M-%f', date('now', '-27 days'));`
+
+const logStmt = `
+INSERT INTO "cv_gen_tracking"(email, cv_hash, origin, step, data, stacktrace) VALUES(
+	?,
+	?,
+	?,
+	?,
+	?,
+	?
+);`
