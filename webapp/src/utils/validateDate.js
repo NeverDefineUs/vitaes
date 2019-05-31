97c425853f6aa7c379d7fbcee387597da3a08274
acrc2 <32888196+acrc2@users.noreply.github.com>
2019-05-31 19:42:39 -0300

    fixing invalid bithday formats (#359)
    * fixing invalid bithday formats
    
    * invalid bithday formats lint fix

--- webapp/src/utils/validateDate.js
+++ webapp/src/utils/validateDate.js
@@ -3,10 +3,14 @@ export default function validateDate(dateStr) {
   const day = Number(dateStr[8] + dateStr[9]);
   const month = Number(dateStr[5] + dateStr[6]);
   const year = Number(dateStr[0] + dateStr[1] + dateStr[2] + dateStr[3]);
+  const firstSpacer = dateStr[4];
+  const secondSpacer = dateStr[7];
   if (
     date.getUTCMonth() + 1 !== month
     || date.getUTCDate() !== day
     || date.getUTCFullYear() !== year
+    || firstSpacer !== '-'
+    || secondSpacer !== '-'
   ) {
     return false;
   }
