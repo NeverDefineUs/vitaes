f8c65412d3256d7fb35ef171a164828b2cdd26ab
artorneira <acrc2@cin.ufpe.br>
2019-05-31 19:15:23 -0300

    invalid bithday formats lint fix

--- webapp/src/utils/validateDate.js
+++ webapp/src/utils/validateDate.js
@@ -3,14 +3,14 @@ export default function validateDate(dateStr) {
   const day = Number(dateStr[8] + dateStr[9]);
   const month = Number(dateStr[5] + dateStr[6]);
   const year = Number(dateStr[0] + dateStr[1] + dateStr[2] + dateStr[3]);
-  const first_spacer = dateStr[4];
-  const second_spacer = dateStr[7];
+  const firstSpacer = dateStr[4];
+  const secondSpacer = dateStr[7];
   if (
     date.getUTCMonth() + 1 !== month
     || date.getUTCDate() !== day
     || date.getUTCFullYear() !== year
-    || first_spacer != "-"
-    || second_spacer != "-"
+    || firstSpacer !== '-'
+    || secondSpacer !== '-'
   ) {
     return false;
   }
