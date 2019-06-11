b6e032c12fac2a57099beabd2714566ff4cdd6a7
Ramon Saboya <ramonsaboya@fb.com>
2019-06-11 11:58:50 -0300

    add linkerd (#371)

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,16 +3,16 @@ if [ "$VITAES_ENV" == "production" ] || [ "$VITAES_ENV" == "staging" ];
 then
     echo "Starting in $VITAES_ENV environment..."
     echo ""
-    kubectl apply --recursive -f kubernetes/rabbit.yaml
-    kubectl apply --recursive -f kubernetes/redis.yaml
-    kubectl apply --recursive -f kubernetes/grafana.yaml
-    kubectl apply --recursive -f kubernetes/logger.yaml
-    kubectl apply --recursive -f kubernetes/webapp.yaml
+    linkerd inject kubernetes/rabbit.yaml | kubectl apply -f -
+    linkerd inject kubernetes/redis.yaml | kubectl apply -f -
+    linkerd inject kubernetes/grafana.yaml | kubectl apply -f -
+    linkerd inject kubernetes/logger.yaml | kubectl apply -f -
+    linkerd inject kubernetes/webapp.yaml | kubectl apply -f -
     sleep 10
-    kubectl apply --recursive -f kubernetes/renderer.yaml
-    kubectl apply --recursive -f kubernetes/api.yaml
-    kubectl apply --recursive -f kubernetes/storage.yaml
-    kubectl apply --recursive -f kubernetes/ingress.yaml
+    linkerd inject kubernetes/renderer.yaml | kubectl apply -f -
+    linkerd inject kubernetes/api.yaml | kubectl apply -f -
+    linkerd inject kubernetes/storage.yaml | kubectl apply -f -
+    linkerd inject kubernetes/ingress.yaml | kubectl apply -f -
     echo ""
     echo "Started in $VITAES_ENV environment"
 else
