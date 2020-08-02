b1905269f059a90d9a3d0b0bda4d98b8cc954c0b
Ramon Saboya <ramonsaboyag@gmail.com>
2020-08-02 17:10:02 -0300

    commit 2

--- renderer/Common.py
+++ renderer/Common.py
@@ -8,6 +8,7 @@ import pika
 import glob
 from fieldy import Encoder, SchemaManager
 
+
 def id_gen(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
