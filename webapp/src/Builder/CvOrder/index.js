5a3ef40f4e4cb8e829a92e21b954852f3dee8c17
Hildemir <hildemir.regis@hotmail.com>
2020-08-02 14:11:52 -0300

    Required changes.

--- webapp/src/Builder/CvOrder/index.js
+++ webapp/src/Builder/CvOrder/index.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import PropTypes from 'prop-types';
-import Card from "react-bootstrap/Card";
+import Card from 'react-bootstrap/Card';
 
 import { translate } from 'i18n/locale';
 
 const SortableItem = SortableElement(({ value }) => (
-  <Card bg="secondary" border="white" text="white" style={{ width: '200%' }} className="text-center p-2">{value}</Card>
+  <Card bg="secondary" text="white" style={{ width: '200%', margin: '1px' }} className="text-center p-2">{value}</Card>
 ));
 
 const SortableList = SortableContainer(({ cvOrder }) => (
   <div style={{
-    display: "block",
-    justifyContent: "center",
-    alignItems: "center"
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   }}>
-  <ul className="Base-orderlist">
-    {cvOrder.map((value, index) => (
-      <SortableItem key={`item-${value}`} index={index} value={translate(value)} />
-    ))}
-  </ul>
+      <ul className="Base-orderlist">
+        {cvOrder.map((value, index) => (
+          <SortableItem key={`item-${value}`} index={index} value={translate(value)} />
+        ))}
+      </ul>
   </div>
 ));
 
